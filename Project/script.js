f9f3f303d766478f8f30b37da7fd79961d06e576
Rachelhu2710 <S10247778@connect.np.edu.sg>
2023-11-15 23:45:13 +0800

    fully functioning chatbot

--- Project/script.js
+++ Project/script.js
@@ -1,41 +1,60 @@
-const url = "https://api.openai.com/v1/chat/completions";
-let ans = []
-let qn = []
+//speech recognition input
+var text = []
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+let recognition = new SpeechRecognition()
+recognition.lang = "en-US"
+
+recognition.onstart = () => {
+    console.log("starting listening, speak in microphone")
+};
+
+recognition.onspeechend = () => {
+    console.log("stopped listening")
+    recognition.stop()
+}
+
+recognition.onresult = (result) => {
+    const vocalInput = result.results[0][0].transcript
+    document.getElementById("qn").innerHTML += vocalInput + "<br>"
+    console.log(vocalInput)
+    callChatGPT(vocalInput)
+}
+document.getElementById("audio").addEventListener('mousedown', function()
+{
+    recognition.start()
+})  
+document.getElementById("audio").addEventListener('mouseup', function()
+{
+    recognition.stop()
+}) 
 //post request to chatgpt
+const url = "https://api.openai.com/v1/chat/completions"
 function callChatGPT(input) {
     fetch(url, {
         "method": "POST",
         "headers": new Headers({
             "Content-Type": "application/json",
-            "Authorization": "Bearer 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"
+            "Authorization": "Bearer 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"
         }),
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
-            messages: [{"role": "user", "content": input}],
+            messages: [{"role": "user", "content": "Pretend you are an OCBC staff."}],
             temperature: 0.7,
         })
     }).then((res) => {
         res.json().then((data) => {
-            ans.push(data.choices[0].message.content)
-            console.log(ans)
-            textdisplay()
+            console.log(data.choices[0].message.content)
+            document.getElementById("responses").innerHTML += data.choices[0].message.content + "<br>"
+            let text = new SpeechSynthesisUtterance(data.choices[0].message.content);
+            speechSynthesis.speak(text);
         })
     })
-}
-function textdisplay(){
-    for (i = 0; i < ans&&qn; ++i) {
-        document.getElementById("responses").innerHTML = ans[i]
-        document.getElementById("qn").innerHTML = qn[i]
-    }
-    // document.getElementById("responses").innerHTML = ans[count]
-    // document.getElementById("qn").innerHTML = qn[count]
-    // if ( anscount&qncount <= count){
-    // count +=1
 }
 
 document.getElementById("submit").addEventListener('click', function()
 {
     var questions = document.getElementById("Question").value
-    qn.push(questions)
+    document.getElementById("qn").innerHTML += questions + "<br>"
+    console.log(questions)
     callChatGPT(questions)
 })  
